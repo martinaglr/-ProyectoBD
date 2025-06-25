1d262d11578897a76547613aee9d4b5008339e7f
jvxcus <147452617+jvxcus@users.noreply.github.com>
2025-06-25 13:04:56 -0400

    Update data10_traduccion.py

--- data10_traduccion.py
+++ data10_traduccion.py
@@ -1,10 +1,12 @@
-from faker import Faker
-import random
-from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator, exceptions
+from pathlib import Path
+from time import sleep
 import unicodedata
-import time
+import random
+execution_path = Path(__file__).parent.absolute()
+script_file_path = Path(execution_path, 'script.sql')
 
-detalles = [
+details = [
     "Cambio de aceite y filtro", "Reemplazo de frenos", "Alineación y balanceo",
     "Revisión de suspensión", "Cambio de batería", "Reparación de transmisión",
     "Inspección de sistema eléctrico", "Reemplazo de bujías", "Revisión de sistema de refrigeración",
@@ -15,31 +17,45 @@ detalles = [
     "Reparación de sistema de escape", "Mantenimiento de sistema de climatización"
 ]
 
-idiomas = ['es', 'en', 'fr', 'ar', 'zh-CN', 'ru']
-fake = Faker('es_CL')
+languages = ['es', 'en', 'fr', 'ar', 'zh-CN', 'ru']
 
-with open('script_carga_traduccion.sql', 'w', encoding='utf-8') as f:
-    for i in range(1, 101):  # CAMBIA 101 a 20001 cuando todo funcione
-        id_orden = random.randint(1, 20001)
-        id_cliente = random.randint(1, 20001)
-        idioma_codigo = random.choice(idiomas)
-        texto_original = random.choice(detalles)
-        texto_original = unicodedata.normalize('NFKD', texto_original)
 
-        if idioma_codigo == 'es':
-            texto_traducido = texto_original
-        else:
-            try:
-                texto_traducido = GoogleTranslator(source='es', target=idioma_codigo).translate(texto_original)
-            except Exception as e:
-                print(f"[ERROR] No se pudo traducir '{texto_original}' a {idioma_codigo}: {e}")
-                texto_traducido = texto_original
+with open(file=script_file_path, mode='w', encoding='utf-8') as script_file:
+    limit = 15_000
+    index = 0
 
-        texto_traducido = texto_traducido.replace("'", "''")
+    while True:
+        if index == limit:
+            break
 
-        f.write(f"INSERT INTO Traduccion (id, id_orden, id_cliente, idioma, texto) "
-                f"VALUES ({i}, {id_orden}, {id_cliente}, '{idioma_codigo}', '{texto_traducido}');\n")
+        index+=1
+        order_id = random.randint(a=1, b=15_001)
+        client_id = random.randint(a=1, b=15_001)
+        code_language = random.choice(seq=languages)
+        original_text = random.choice(seq=details)
+        original_text = unicodedata.normalize('NFKD', original_text)
 
-        print(f"[OK] #{i} '{texto_original}' -> ({idioma_codigo}) '{texto_traducido}'")
 
-        time.sleep(0.3)  # control para evitar bloqueo
+        # se maneja la excepción de TooManyRequests
+        # en caso de que se sature la API a la cual
+        # se consulta para la traducción
+
+        # Server Error: You made too many requests to the server.According to google, 
+        # you are allowed to make 5 requests per secondand up to 200k requests per day.
+        # You can wait and try again later oryou can try the translate_batch function
+        try:
+            translated_text = (GoogleTranslator(source='es', target=code_language).translate(text=original_text) if code_language != 'es' else original_text).replace("'", "''")
+        except exceptions.TooManyRequests as e:
+            translated_text = original_text     # valor por defecto
+            #continue                           # escribirá sólo en ES
+        #
+        # para probar si este código llega hasta las 200.000 filas,
+        # dejar habilitada esta declaración y dejar comentadas las anteriores
+        #translated_text = original_text
+        
+        sql_statement = f'''INSERT INTO traduccion (id, id_orden, id_client, idioma, texto) VALUES ({index}, {order_id}, {client_id}, '{code_language}', '{translated_text}');\n'''
+        script_file.write(sql_statement)
+
+
+        sleep(0.3)
+
